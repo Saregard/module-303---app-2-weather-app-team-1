7aadfd97c8beda7e4101837dc54c0b0b9d7f7a4a
Saregard <77791326+Saregard@users.noreply.github.com>
2021-06-10 17:24:25 +0200

    Added imports
    Added imports for backend files

--- app/src/main/java/com/example/firstactivity/backend/Endpoint.kt
+++ app/src/main/java/com/example/firstactivity/backend/Endpoint.kt
@@ -1,7 +1,10 @@
 package com.example.firstactivity.backend
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface Endpoint {
 
-    @GET("data/2.5/weather?q={city name}&appid={API key}")
-    fun getWeatherData(@Path("city name") city: String)
+    @GET("data/2.5/weather?q={cityname}&appid={APIkey}")
+    fun getWeatherData(@Query("cityname") city: String, @Query("APIkey") API: String)
 }
