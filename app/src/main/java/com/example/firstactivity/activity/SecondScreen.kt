efea34b2298dedab788c3c83e8fb8380bac9f48f
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-06-15 14:38:06 +0200

    Second Screen Progress

--- app/src/main/java/com/example/firstactivity/activity/SecondScreen.kt
+++ app/src/main/java/com/example/firstactivity/activity/SecondScreen.kt
@@ -1,27 +1,33 @@
 package com.example.firstactivity.activity
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.example.firstactivity.R
+import com.example.firstactivity.databinding.ActivitySecondScreenBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SecondScreen : AppCompatActivity() {
-    private var textViewDate: TextView? = null
-    private var textViewTime: TextView? = null
-
+    lateinit var binding: ActivitySecondScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_screen)
+        setContentView(binding.root)
+
+        setDateAndTime()
     }
     private fun setDateAndTime() {
-        textViewDate = findViewById(R.id.Date)
-        textViewTime = findViewById(R.id.Time)
         val currentDate = Date()
-        val timeFormat = SimpleDateFormat("hh:mm a")
-        val dateFormat = SimpleDateFormat("dd MMMM yyyy")
-        
+
+        val timeToShow = SimpleDateFormat("hh:mm a").format(currentDate)
+        binding.Time.text = timeToShow
+
+        val dateToShow = SimpleDateFormat("dd MMMM yyyy").format(currentDate)
+        binding.Date.text = dateToShow
     }
+
+    private fun
+
 
 }
